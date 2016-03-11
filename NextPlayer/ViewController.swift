84488e189d40e7da791f77a202144eff30202cd3
靳洪博 <aifjhb0815@hotmail.com>
2016-03-11 12:26:52 +0800

    -init the project

--- NextPlayer/ViewController.swift
+++ NextPlayer/ViewController.swift
@@ -7,14 +7,20 @@
 //
 
 import UIKit
+import KVNProgress
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        KVNProgress.setConfiguration(KVNProgressConfiguration.defaultConfiguration())
     }
 
+    override func viewDidAppear(animated: Bool) {
+        KVNProgress.showWithStatus("This is a test ")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
