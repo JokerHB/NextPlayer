a8638898cb0fa6eb739beeebd6cef6591dd62d75
靳洪博 <aifjhb0815@hotmail.com>
2016-03-11 08:48:50 +0800

    Initial Commit

---
+++ NextPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NextPlayer
+//
+//  Created by 靳洪博 on 16/3/11.
+//  Copyright © 2016年 joker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
